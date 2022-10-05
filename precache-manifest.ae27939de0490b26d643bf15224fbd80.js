673c1502fb720c5e22ecd01b92c20ce6d0bc1b8a
Jack <1162858020@qq.com>
2022-10-05 21:20:13 +0800

    deploy

---
+++ precache-manifest.ae27939de0490b26d643bf15224fbd80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c09ac2df5b06c1cbb18",
+    "url": "/money-web/css/app.0955ad1f.css"
+  },
+  {
+    "revision": "a0e95266a04f8c40f3440edecf3599c2",
+    "url": "/money-web/index.html"
+  },
+  {
+    "revision": "0c09ac2df5b06c1cbb18",
+    "url": "/money-web/js/app.347a1af2.js"
+  },
+  {
+    "revision": "b8a63fa27e8826f3ac88",
+    "url": "/money-web/js/chunk-vendors.49abd244.js"
+  },
+  {
+    "revision": "723534453f7ed08651c3ec25066c677d",
+    "url": "/money-web/manifest.json"
+  },
+  {
+    "revision": "08da34d92d3ba309261e177cda3e21a3",
+    "url": "/money-web/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-web/robots.txt"
+  }
+]);
